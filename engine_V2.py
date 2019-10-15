7de62c8eb53e5767044bf57cdbc2ea17a780edc7
lykcas <841029817@qq.com>
2019-10-15 21:27:19 +0100

    work done

--- engine_V2.py
+++ engine_V2.py
@@ -94,127 +94,145 @@ def distence_search(my_index, dis_a, dis_b, dis):
 
 
 def search_active(stops, my_index):
-    f_ = 
-    str_input = input("Please input: ")
-    hash_ = re.compile("#")
-    whether_hash = hash_.match(str_input)
-    whether_space = 0
-    whether_and = str_input.find('AND')
-    whether_or = str_input.find('OR')
-    whether_not = str_input.find('AND NOT')
-    whether_bool = 0
-    if whether_and > 1 or whether_or > 1 or whether_not > 1:
-        whether_bool = 1
+    f_ = open('./query_bool.txt')
+    f__ = open('boolean.txt', 'w')
+    lines = f_.readlines()
+    for line in lines:
+        line = line.strip('\n')
+        pos1 = line.find(' ')
+        query_number = line[1:pos1-1]
+        str_input = line[pos1+1:]
+        result = []
 
-    for i in str_input:
-        if i == ' ':
-            whether_space = 1
-    if whether_hash:  # 距离搜索
-        dis = []
+        # str_input = input("Please input: ")
+        hash_ = re.compile("#")
+        whether_hash = hash_.match(str_input)
+        whether_space = 0
+        whether_and = str_input.find('AND')
+        whether_or = str_input.find('OR')
+        whether_not = str_input.find('AND NOT')
+        whether_bool = 0
+        if whether_and > 1 or whether_or > 1 or whether_not > 1:
+            whether_bool = 1
+
         for i in str_input:
-            if i == '(':
-                break
-            if i != '#':
-                dis.append(i)
-        dis_str = "".join(dis)
-        dis_int = int(dis_str)
-        pos1 = str_input.find('(')
-        pos2 = str_input.find(',')
-        pos3 = str_input.find(')')
-        dis_a = str_input[pos1 + 1:pos2]
-        dis_b = str_input[pos2 + 2:pos3]
-        dis_a = term_manage(dis_a, stops)
-        dis_b = term_manage(dis_b, stops)
-        return distence_search(my_index, dis_a, dis_b, dis_int)
-        # exit()
-    elif whether_space == 0:  # 单个单词搜索
-        term_managed = term_manage(str_input, stops)
-        return one_term_search(my_index, term_managed)
-    elif str_input[0] == '"' and str_input[-1] == '"' and whether_bool == 0:  # phrase_search短语搜索
-        phrase_str = str_input[1:-1]
-        phrase_str = phrase_str.split()
-        phrase_a = term_manage(phrase_str[0], stops)
-        phrase_b = term_manage(phrase_str[1], stops)
-        return phrase_search(my_index, phrase_a, phrase_b)
+            if i == ' ':
+                whether_space = 1
+        if whether_hash:  # 距离搜索
+            dis = []
+            for i in str_input:
+                if i == '(':
+                    break
+                if i != '#':
+                    dis.append(i)
+            dis_str = "".join(dis)
+            dis_int = int(dis_str)
+            pos1 = str_input.find('(')
+            pos2 = str_input.find(',')
+            pos3 = str_input.find(')')
+            dis_a = str_input[pos1 + 1:pos2]
+            dis_b = str_input[pos2 + 2:pos3]
+            dis_a = term_manage(dis_a, stops)
+            dis_b = term_manage(dis_b, stops)
+            result = distence_search(my_index, dis_a, dis_b, dis_int)
+            # exit()
+        elif whether_space == 0:  # 单个单词搜索
+            term_managed = term_manage(str_input, stops)
+            result = one_term_search(my_index, term_managed)
+        elif str_input[0] == '"' and str_input[-1] == '"' and whether_bool == 0:  # phrase_search短语搜索
+            phrase_str = str_input[1:-1]
+            phrase_str = phrase_str.split()
+            phrase_a = term_manage(phrase_str[0], stops)
+            phrase_b = term_manage(phrase_str[1], stops)
+            result = phrase_search(my_index, phrase_a, phrase_b)
 
-    elif whether_bool == 1:
-        dict1 = []
-        dict2 = []
-        if whether_and > 0 and whether_not < 0:  # AND
-            sear_type = 1
-            pos = whether_and
-            if str_input[0] == '"':
-                phrase_part = str_input[1:pos-2]
-                phrase_part = phrase_part.split()
-                phrase_a = term_manage(phrase_part[0], stops)
-                phrase_b = term_manage(phrase_part[1], stops)
-                dict1 = phrase_search(my_index, phrase_a, phrase_b)
-            elif str_input[0] != '"':
-                term_a = str_input[0:pos-1]
-                term_a = term_manage(term_a, stops)
-                dict1 = one_term_search(my_index, term_a)
-            if str_input[-1] == '"':
-                phrase_part = str_input[pos+5:-1]
-                phrase_part = phrase_part.split()
-                phrase_a = term_manage(phrase_part[0], stops)
-                phrase_b = term_manage(phrase_part[1], stops)
-                dict2 = phrase_search(my_index, phrase_a, phrase_b)
-            elif str_input[-1] != '"':
-                term_b = str_input[pos+4:]
-                term_b = term_manage(term_b, stops)
-                dict2 = one_term_search(my_index, term_b)
-            return bool_term_search(my_index, dict1, sear_type, dict2)
-        elif whether_or > 0:  # OR
-            sear_type = 2
-            pos = whether_or
-            if str_input[0] == '"':
-                phrase_part = str_input[1:pos-2]
-                phrase_part = phrase_part.split()
-                phrase_a = term_manage(phrase_part[0], stops)
-                phrase_b = term_manage(phrase_part[1], stops)
-                dict1 = phrase_search(my_index, phrase_a, phrase_b)
-            elif str_input[0] != '"':
-                term_a = str_input[0:pos-1]
-                term_a = term_manage(term_a, stops)
-                dict1 = one_term_search(my_index, term_a)
-            if str_input[-1] == '"':
-                phrase_part = str_input[pos+4:-1]
-                phrase_part = phrase_part.split()
-                phrase_a = term_manage(phrase_part[0], stops)
-                phrase_b = term_manage(phrase_part[1], stops)
-                dict2 = phrase_search(my_index, phrase_a, phrase_b)
-            elif str_input[-1] != '"':
-                term_b = str_input[pos+3:]
-                term_b = term_manage(term_b, stops)
-                dict2 = one_term_search(my_index, term_b)
-            return bool_term_search(my_index, dict1, sear_type, dict2)
-        elif whether_not > 0:
-            sear_type = 3
-            pos = whether_not
-            if str_input[0] == '"':
-                phrase_part = str_input[1:pos - 2]
-                phrase_part = phrase_part.split()
-                phrase_a = term_manage(phrase_part[0], stops)
-                phrase_b = term_manage(phrase_part[1], stops)
-                dict1 = phrase_search(my_index, phrase_a, phrase_b)
-            elif str_input[0] != '"':
-                term_a = str_input[0:pos - 1]
-                term_a = term_manage(term_a, stops)
-                dict1 = one_term_search(my_index, term_a)
-            if str_input[-1] == '"':
-                phrase_part = str_input[pos + 9:-1]
-                phrase_part = phrase_part.split()
-                phrase_a = term_manage(phrase_part[0], stops)
-                phrase_b = term_manage(phrase_part[1], stops)
-                dict2 = phrase_search(my_index, phrase_a, phrase_b)
-            elif str_input[-1] != '"':
-                term_b = str_input[pos + 8:]
-                term_b = term_manage(term_b, stops)
-                dict2 = one_term_search(my_index, term_b)
-            return bool_term_search(my_index, dict1, sear_type, dict2)
+        elif whether_bool == 1:
+            dict1 = []
+            dict2 = []
+            if whether_and > 0 and whether_not < 0:  # AND
+                sear_type = 1
+                pos = whether_and
+                if str_input[0] == '"':
+                    phrase_part = str_input[1:pos-2]
+                    phrase_part = phrase_part.split()
+                    phrase_a = term_manage(phrase_part[0], stops)
+                    phrase_b = term_manage(phrase_part[1], stops)
+                    dict1 = phrase_search(my_index, phrase_a, phrase_b)
+                elif str_input[0] != '"':
+                    term_a = str_input[0:pos-1]
+                    term_a = term_manage(term_a, stops)
+                    dict1 = one_term_search(my_index, term_a)
+                if str_input[-1] == '"':
+                    phrase_part = str_input[pos+5:-1]
+                    phrase_part = phrase_part.split()
+                    phrase_a = term_manage(phrase_part[0], stops)
+                    phrase_b = term_manage(phrase_part[1], stops)
+                    dict2 = phrase_search(my_index, phrase_a, phrase_b)
+                elif str_input[-1] != '"':
+                    term_b = str_input[pos+4:]
+                    term_b = term_manage(term_b, stops)
+                    dict2 = one_term_search(my_index, term_b)
+                result = bool_term_search(my_index, dict1, sear_type, dict2)
+            elif whether_or > 0:  # OR
+                sear_type = 2
+                pos = whether_or
+                if str_input[0] == '"':
+                    phrase_part = str_input[1:pos-2]
+                    phrase_part = phrase_part.split()
+                    phrase_a = term_manage(phrase_part[0], stops)
+                    phrase_b = term_manage(phrase_part[1], stops)
+                    dict1 = phrase_search(my_index, phrase_a, phrase_b)
+                elif str_input[0] != '"':
+                    term_a = str_input[0:pos-1]
+                    term_a = term_manage(term_a, stops)
+                    dict1 = one_term_search(my_index, term_a)
+                if str_input[-1] == '"':
+                    phrase_part = str_input[pos+4:-1]
+                    phrase_part = phrase_part.split()
+                    phrase_a = term_manage(phrase_part[0], stops)
+                    phrase_b = term_manage(phrase_part[1], stops)
+                    dict2 = phrase_search(my_index, phrase_a, phrase_b)
+                elif str_input[-1] != '"':
+                    term_b = str_input[pos+3:]
+                    term_b = term_manage(term_b, stops)
+                    dict2 = one_term_search(my_index, term_b)
+                result = bool_term_search(my_index, dict1, sear_type, dict2)
+            elif whether_not > 0:
+                sear_type = 3
+                pos = whether_not
+                if str_input[0] == '"':
+                    phrase_part = str_input[1:pos - 2]
+                    phrase_part = phrase_part.split()
+                    phrase_a = term_manage(phrase_part[0], stops)
+                    phrase_b = term_manage(phrase_part[1], stops)
+                    dict1 = phrase_search(my_index, phrase_a, phrase_b)
+                elif str_input[0] != '"':
+                    term_a = str_input[0:pos - 1]
+                    term_a = term_manage(term_a, stops)
+                    dict1 = one_term_search(my_index, term_a)
+                if str_input[-1] == '"':
+                    phrase_part = str_input[pos + 9:-1]
+                    phrase_part = phrase_part.split()
+                    phrase_a = term_manage(phrase_part[0], stops)
+                    phrase_b = term_manage(phrase_part[1], stops)
+                    dict2 = phrase_search(my_index, phrase_a, phrase_b)
+                elif str_input[-1] != '"':
+                    term_b = str_input[pos + 8:]
+                    term_b = term_manage(term_b, stops)
+                    dict2 = one_term_search(my_index, term_b)
+                result = bool_term_search(my_index, dict1, sear_type, dict2)
 
-    else:
-        return 'No result aaa'
+        else:
+            result = 'No result'
+
+        if result == 'No result':
+            f__.write(query_number + ' 0 0 0 1 0\n')
+        else:
+            for ii in result:
+                f__.write(query_number + ' 0 ' + str(ii) + ' 0 1 0')
+                f__.write('\n')
+    f_.close()
+    f__.close()
 
 
 if __name__ == '__main__':
@@ -277,8 +295,7 @@ if __name__ == '__main__':
         fileObject.write('\n')
     fileObject.close()
 
-    while 1:
-        print(search_active(stops, my_index))
+    search_active(stops, my_index)
 
 
 
